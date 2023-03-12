f3f0692bdc4128b086f24fdaca0c2d51a8d9fb43
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-03-12 15:46:23 -0400

    App and Server added

---
+++ app/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const todosRouter = require('../router/todosRouter');
+const usersRouter = require('../router/usersRouter')
+const app = express();
+
+// http://localhost:3000
+app.get("/", (req, res, next) =>{
+  res.status(200).json({message:'Service is up'})
+})
+
+// router middleware
+app.use("/todos",todosRouter)
+app.use("/users", usersRouter)
+
+// add middleware to handle errors and bad url paths
+app.use((req, res, next) =>{
+  const error = new Error("NOT FOUND");
+  error.status = 404;
+  next(error)
+});
+
+app.use((error, req, res, next) => {
+ res.status(error.status || 500).json({
+    error: {
+      message: error.message,
+      status: error.status,
+      method: req.method,
+    },
+ });
+});
+
+module.exports = app
